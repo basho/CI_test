13b114e74c6f3e9fd0609b8a441799195d915522
Andrew Thompson <andrew@hijacked.us>
2014-02-20 16:12:25 -0500

    Add some stuff

---
+++ src/ci_test.erl
@@ -0,0 +1,19 @@
+-module(ci_test).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-export([main/0]).
+
+main() ->
+    ok.
+
+-ifdef(TEST).
+simple_test() ->
+    ?assert(true).
+
+silly_test() ->
+    ?assertEqual(true, self() == self()).
+
+-endif.
